8d2f4cd81b7e17636364219790400b9a1669dc07
root <root@Cleanmaster>
2023-07-05 18:50:03 +0300

    Create a program that prints the various datatypes available in C++.

---
+++ datatype.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    //Prints the size of various data types.
+    cout << "Size of data types:" << endl << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    cout << "Size of long: " << sizeof(long) << " bytes" << endl;
+    cout << "Size of boolean: " << sizeof(bool) << " bytes" << endl;
+    cout << "Size of long double: " << sizeof(long double) << " bytes" << endl;
+    cout << "Size of short: " << sizeof(short) << " bytes" << endl;
+    cout << "Size of short int: " << sizeof(short int) << " bytes" << endl;
+    cout << "Size of long int: " << sizeof(long int) << " bytes" << endl;
+
+    return 0;
+}
