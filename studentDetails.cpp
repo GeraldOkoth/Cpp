e9361f9b3c72ffe7b9b853dce8ff94bf7c473475
TechiesKE <okothgerald449@gmail.com>
2024-05-01 17:32:55 +0300

    Modify functions

--- studentDetails.cpp
+++ studentDetails.cpp
@@ -1,40 +1,51 @@
 #include<iostream>
 
 using namespace std;
+
 class student
 {
 private:
-	int admno;
+	string admno;
 	string sname;
-	float eng ,math, science;
-	float total;
+	string c1, c2, c3, c4, c5, c6, c7;
+
+	/*Private function to output total marks
 	float ctotal()
 	{
 		return eng + math + science;
-	}
+	}*/
 public:
 	void takeData()
 	{
-		cout << "Enter admission number: ";
+	    cout << "Student name: ";
+	    getline(cin, sname);
+		cout << "Admission number: ";
 		cin >> admno;
-		cout << "Enter student name: ";
-		cin >> sname; //getline(cin, sname);
-		cout << "Enter marks in english, math, science "  << endl;
-		cin >> eng >> math >> science;
-		total = ctotal();
+		cout << "Please enter at least 3 and at most 7 courses: "  << endl;
+		cin >> c1 >> c2 >> c3 >> c4 >> c5 >> c6 >> c7;
 	}
-	void Showdata()
+	void showData()
 	{
-		cout << "Admission number " << admno << "\nStudent name " << sname << "\nEnglish "
-			<< eng << "\nMath "<< math <<"\nScience " << science << "\nTotal " << total;
+		cout << "Student name: " << sname << endl;
+		cout << "Admission number: " << admno << endl;
+		cout << c1 << endl << c2 << endl << c3 << endl << c4 << endl << c5 << endl << c6 << endl << c7 << endl << endl;
 	}
 };
+
 int main ()
 {
+    cout << "\t\t\t\t\t\t======Student Management System======" <<endl;
+    cout << "\t\t\t\t\t\t      -------------------------" <<endl;
+
+    student obj;
+
+    cout << "Please enter student details: " <<endl <<endl;
+    obj.takeData();
+    cout << "You entered the following details: " <<endl <<endl;
+    obj.showData();
+
 	//clrscr();
-	student obj ;
-	obj.takeData();
-	obj.Showdata();
-	//getch();
+    //getch();
+
 	return 0;
 }
